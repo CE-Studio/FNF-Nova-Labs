a821e85acd693258dc01d10c53c607f9384a2dac
clarence112 <clarencepacelli@gmail.com>
2023-01-12 23:56:37 -0500

    get things to the point of compiling

---
+++ setup.sh
@@ -0,0 +1,21 @@
+haxelib install hmm
+haxelib run hmm install
+haxelib install lime
+haxelib install openfl
+haxelib install flixel
+haxelib run lime setup flixel
+haxelib run lime setup
+haxelib install flixel-tools
+haxelib run flixel-tools setup
+haxelib update flixel
+haxelib install flixel-addons
+haxelib install flixel-ui
+haxelib install hscript
+haxelib install newgrounds
+haxelib git hxCodec https://github.com/polybiusproxy/hxCodec.git
+haxelib update flixel-addons
+haxelib update flixel-ui
+haxelib update hscript
+haxelib update newgrounds
+haxelib git polymod https://github.com/larsiusprime/polymod.git
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
